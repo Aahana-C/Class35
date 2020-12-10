7d3c0abcd5e7d903c0651eac7fde9c46070a808c
Aahana-C <aahanacha@gmail.com>
2020-12-10 16:59:06 +0000

    added

--- sketch.js
+++ sketch.js
@@ -1,29 +1,48 @@
 var ball;
+var database;
+var  balldb;
+var position;
 
 function setup(){
     createCanvas(500,500);
+
+    database = firebase.database();
+
     ball = createSprite(250,250,10,10);
     ball.shapeColor = "red";
+//ref - point db, on - listen (values - pos - x&y)
+    balldb = database.ref('ball/position');
+    balldb.on("value",readPosition);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
-function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+//write to the database ( arrow key pressed)
+function writePosition(x,y){
+database.ref('ball/position').set({
+    x : position.x + x,
+    y : position.y + y
+})
+}
+
+//read from the database - x-200,y -200
+function readPosition(data){
+position = data.val();
+ball.x = position.x;
+ball.y = position.y;
 }
